ccca0d655b03997988e2b9d9a1daeeb88b054f9f
unknown <xartemiusx@gmail.com>
2015-09-14 23:02:58 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=manager',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => 'mngr_'
+];
